f09a2be9ca1f503a385df2ade2e86d119c2b5c9e
Ivan Scherbakov <zloboglaz@inbox.ru>
2012-05-23 23:32:10 +0400

    Added configuration support for weekplan

--- weekplan.sh
+++ weekplan.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 source `dirname $0`/libplan.sh
 
+if [ -f $HOME/.weekplanrc ]; then
+	source $HOME/.weekplanrc
+fi
+
+if [ "x$OVERALL_WIDTH" == "x" ]; then
+	OVERALL_WIDTH=50
+fi
+
+if [ "x$TAG_FIELD_WIDTH" == "x" ]; then
+	TAG_FIELD_WIDTH=6
+fi
+
+if [ "x$PX_OFFSET" == "x" ]; then
+	PX_OFFSET=14
+fi
+
+if [ "x$HILITE_COLOR" == "x" ]; then
+	HILITE_COLOR="orange"
+fi
+
+if [ "x$CON_OUTPUT" == "x" ]; then
+	CON_OUTPUT=0	
+fi
+
+	
+TXT_FIELD_WIDTH=`echo $OVERALL_WIDTH - $TAG_FIELD_WIDTH - 1 | bc`
+
 NOW=`date +%Y%m%d`
 
 function print_entry()
@@ -12,17 +39,27 @@ function print_entry()
 	local ATEXT=`echo "$*" | iconv -f utf8 -t cp1251`
 
 	if [ "$STAMP" != "$PREVSTAMP" ]; then
-		printf "\n\n"
-		if [ "$STAMP" == "$NOW" ]; then echo -n "\${color orange}"; fi
-		printf "\${offset 14}%52s" "`date -d "$STAMP" "+%Y-%m-%d"`"
-		if [ "$STAMP" == "$NOW" ]; then echo -n "\${color}"; fi
-		printf "\n"
+		local TEMPLATE="%$OVERALL_WIDTH""s"
+
+		if [ $CON_OUTPUT -ne 1 ]; then
+			if [ "$STAMP" == "$NOW" ]; then 
+				TEMPLATE="\${offset $PX_OFFSET}\${color $HILITE_COLOR}$TEMPLATE\${color}"
+			else
+				TEMPLATE="\${offset $PX_OFFSET}$TEMPLATE"
+			fi
+		fi
+
+		printf "\n\n""$TEMPLATE""\n" `date -d "$STAMP" "+%Y-%m-%d"`
 
 		PREVSTAMP=$STAMP
 	fi
 
+	if [ $CON_OUTPUT -ne 1 ]; then
+		echo -n "\${offset $PX_OFFSET}"
+	fi
+
 # Restoring UTF-8 text	
-	printf "\${offset 14}%-45s %6s\n" "$ATEXT" "$GROUP" | iconv -f cp1251 -t utf8
+	printf "%-$TXT_FIELD_WIDTH""s %$TAG_FIELD_WIDTH""s\n" "$ATEXT" "$GROUP" | iconv -f cp1251 -t utf8
 }
 
 plan_week_entries | while read LINE; do
