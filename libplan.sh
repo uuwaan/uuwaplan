415f5041aab6eecbd7c31d731de1fc605a4b1e0d
Ivan Scherbakov <zloboglaz@inbox.ru>
2012-05-27 22:31:24 +0400

    Code was reworked to use pipelining instead of arguments.

--- libplan.sh
+++ libplan.sh
@@ -3,26 +3,66 @@ if [[ -z `echo $PLAN_DATABASE` ]]; then
 	PLAN_DATABASE="$HOME/.plans.txt"
 fi
 
+# Reads entire database
+function plan_read_lines()
+{
+	cat "$PLAN_DATABASE" | sort
+}
+
 # Filters lines by text pattern 
 function plan_filter_by_text()
 {
-	local FILTER=$1; shift
-	local RESULT=`echo "$*" | awk '/'"$FILTER"'/ { print $0 }'`
+	local LINE
+	local RESULT
+	while read LINE; do
+		if [[ "$LINE" =~ $1 ]]; then
+			echo $LINE
+		fi
+	done
+}
 
-	if [ -n "$RESULT" ]; then
-		echo $RESULT
-	fi
+# Filters lines by importance 
+function plan_filter_by_imp()
+{
+	local LINE
+	local GROUP 
+	while read LINE; do
+		GROUP=`echo "$LINE" | plan_ex_field 2`
+		if [[ -n `plan_is_important $GROUP` ]]; then
+			echo $LINE
+		fi
+	done
 }
 
 # Filters lines by date range 
 function plan_filter_by_date()
 {
-	local DATE_FROM=$1; shift
-	local DATE_TO=$1; shift
+	local LINE
+	local STAMP
+	while read LINE; do
+		STAMP=`echo $LINE | plan_ex_field 1`
+		if (($STAMP >= $1 && $2 >= $STAMP)); then
+			echo $LINE 
+		fi
+	done
+}
 
-	if (($1 >= $DATE_FROM && $1 <= $DATE_TO)); then
-		echo $* 
-	fi
+# Extracts field from lines
+function plan_ex_field()
+{
+	local LINE
+	while read LINE; do
+		echo $LINE | awk "{ print \$$1 }"
+	done
+}
+
+# Cuts the lines on certain position 
+function plan_cut_line()
+{
+	local LINE
+	while read LINE; do
+		echo $LINE | cut -d\  -f$1- 
+	done
 }
 
 # Invokes external editor for entry
@@ -32,30 +72,31 @@ function plan_edit_externally()
 	local SUFFIX=`echo $* $SALT | sha1sum | awk '{print $1}'`
 	local FILE=$PLAN_DATABASE.$SUFFIX
 
-	echo $* > $FILE
+	echo $* > "$FILE"
 	PLAN_LAST_EDIT=""
 	if [[ -n `echo $EDITOR` ]]; then
 # Workaround, as editor wasn't opened with direct connection to terminal
-		$EDITOR $FILE < /dev/tty
+		$EDITOR "$FILE" < /dev/tty
 		if (( 0 == $? )); then
-			PLAN_LAST_EDIT=`cat $FILE | head -1`
+			PLAN_LAST_EDIT=`cat "$FILE" | head -1`
 		fi
 	else
 		echo 'plan_edit_externally: no external editor found in EDITOR variable. Do smth like "export EDITOR=mcedit"'
 	fi
-	rm -f $FILE
+	rm -f "$FILE"
 }
 
 # Creates simple regexp from specified arguments 
 function plan_pattern_from_args()
 {
 # Try to interpret first argument as timestamp to narrow search
-	local TRY_DATE=`plan_is_date "$1"`
+	local PATTERN
+	local TRY_DATE=`plan_parse_date "$1"`
 	if [[ -n "$TRY_DATE" ]]; then
-		local PATTERN=$TRY_DATE".*"
+		PATTERN=$TRY_DATE".*"
 		shift
 	else
-		local PATTERN=".*"
+		PATTERN=".*"
 	fi
 
 	for i in `seq 1 $#`; do
@@ -64,6 +105,31 @@ function plan_pattern_from_args()
 	done
 
 	echo $PATTERN
+}
+
+# Copies lines that didn't match the pattern to file and echoes others. 
+function plan_hook_lines()
+{
+	local LINE
+	while read LINE; do
+		if [[ -n `echo "$LINE" | plan_filter_by_text "$2"` ]]; then
+			echo $LINE
+		else
+			echo $LINE >> $1
+		fi
+	done
+}
+
+# Returns DB timestamp if input can be interpreted as date
+function plan_parse_date()
+{
+	echo "$1" | grep -e '[a-z0-9[:space:]+-]\+' >/dev/null
+	if (( 1 == $? )); then return; fi
+
+	local TRY_DATE=`date +%Y%m%d -d "$1" 2>/dev/null`
+	if (( 1 == $? )); then return; fi
+
+	echo $TRY_DATE
 }
 
 # Shows whether this entry is important or not
@@ -76,67 +142,29 @@ function plan_is_important()
 	fi
 }
 
-# Returns DB timestamp if input can be interpreted as date
-function plan_is_date()
-{
-	echo "$1" | grep -e '[a-z0-9[:space:]+-]\+' >/dev/null
-	if (( 1 == $? )); then return; fi
-
-	local TRY_DATE=`date +%Y%m%d -d "$1" 2>/dev/null`
-	if (( 1 == $? )); then return; fi
-
-	echo $TRY_DATE
-}
-
-# Reads database line by line and invokes callback function
-function plan_read_lines()
-{
-	local LINE
-	local RESULT
-	cat "$PLAN_DATABASE" | sort | while read LINE; do
-		RESULT=$($1 $LINE)
-		if [[ -n "$RESULT" ]]; then
-			echo $RESULT 	
-		fi
-	done
-}
-
-# Reads database line by line, copies lines that didn't match the pattern and echoes others. 
-function plan_hook_lines()
-{
-	local LINE
-	plan_read_lines "echo" | while read LINE; do
-		if [[ -n `plan_filter_by_text "$2" $LINE` ]]; then
-			echo $LINE
-		else
-			echo $LINE >> $1
-		fi
-	done
-}
-
 # Shows dates with active entries in specified month
 function plan_month_dates()
 {
 	local MONTH
 
-	if [[ -n `plan_is_date "$1"` ]]; then
+	if [[ -n `plan_parse_date "$1"` ]]; then
 		MONTH=`date -d "$1" +%m`
 	else
 		MONTH=`date +%m`
 	fi
 
-	local TIME_NOW=`date -d "$MONTH/01" +%Y%m%d`
-	local TIME_END=`date -d "$TIME_NOW -1 day +1 month" +%Y%m%d`
+	local TIMESTART=`date -d "$MONTH/01" +%Y%m%d`
+	local TIMEND=`date -d "$TIMESTART -1 day +1 month" +%Y%m%d`
+
+	local CONVEYER="plan_read_lines | plan_filter_by_date $TIMESTART $TIMEND"
+	if [[ "1" == "$2" ]]; then
+		CONVEYER=$CONVEYER" | plan_filter_by_imp"
+	fi
 
 	local LINE
 	local PREVSTAMP=-1
-	plan_read_lines "plan_filter_by_date $TIME_NOW $TIME_END" | while read LINE; do
-		local STAMP=`echo $LINE | awk '{ print $1 }'`
-		local IS_IMP=$(plan_is_important `echo $LINE | awk '{ print $2 }'`)
-
-		if [[ "1" == "$2" && -z "$IS_IMP" ]]; then
-			continue
-		fi
+	eval "$CONVEYER" | while read LINE; do
+		local STAMP=`echo $LINE | plan_ex_field 1`
 
 		if [[ "$STAMP" != "$PREVSTAMP" ]]; then
 			echo $STAMP
@@ -148,23 +176,23 @@ function plan_month_dates()
 # Shows lines that have date within specified range 
 function plan_date_entries()
 {
-	local TIMESTART=`plan_is_date "$1"`; shift
+	local TIMESTART=`plan_parse_date "$1"`; shift
 	if [[ -z "$TIMESTART" ]]; then
 		echo "plan_date_entries: wrong date format for start date"
 		return
 	fi
 
-	local TIMEND=`plan_is_date "$1"`; shift
+	local TIMEND=`plan_parse_date "$1"`; shift
 	if [[ -z "$TIMEND" ]]; then
 		echo "plan_date_entries: wrong date format for end date"
 		return
 	fi
 
-	local LIMIT=$1; shift
-
+	local CONVEYER="plan_read_lines | plan_filter_by_date $TIMESTART $TIMEND"
 	local LINE
 	local i=0
-	plan_read_lines "plan_filter_by_date $TIMESTART $TIMEND" | while read LINE; do
+	local LIMIT=$1; shift
+	eval "$CONVEYER" | while read LINE; do
 		echo $LINE
 		let i+=1
 		if (( $LIMIT <= $i )); then break; fi
@@ -187,7 +215,7 @@ function plan_add_entry()
 		shift
 	fi
 
-	local TIMESTAMP=`plan_is_date "$1"`; shift
+	local TIMESTAMP=`plan_parse_date "$1"`; shift
 	if [[ -z "$TIMESTAMP" ]]; then
 		echo "plan_add_entry: wrong date format"
 		return
@@ -229,7 +257,7 @@ function plan_add_rep_entry()
 		shift
 	fi
 
-	local TIMESTAMP=`plan_is_date "$1"`; shift
+	local TIMESTAMP=`plan_parse_date "$1"`; shift
 	if [[ -z "$TIMESTAMP" ]]; then
 		echo "plan_add_rep_entry: wrong date format"
 		return
@@ -255,7 +283,7 @@ function plan_add_rep_entry()
 # Moves entry to new date
 function plan_move_entry()
 {
-	local NEW_DATE=`plan_is_date "$1"`; shift
+	local NEW_DATE=`plan_parse_date "$1"`; shift
 	if [[ -z "$NEW_DATE" ]]; then
 		echo "plan_move_entry: wrong date format"
 		return
@@ -269,9 +297,9 @@ function plan_move_entry()
 	fi
 
 	local LINE
-	plan_hook_lines "$DB_FILE" "$PATTERN" | while read LINE; do
-		local REST=`echo $LINE | cut -d\  -f2-`
-		echo $NEW_DATE $REST >> "$DB_FILE"
+	local CONVEYER="plan_read_lines | plan_hook_lines \"$DB_FILE\" \"$PATTERN\" | plan_cut_line 2"
+	eval "$CONVEYER" | while read LINE; do
+		echo $NEW_DATE $LINE >> "$DB_FILE"
 	done
 
 	mv "$DB_FILE" "$PLAN_DATABASE"
@@ -288,10 +316,9 @@ function plan_edit_entry()
 	fi
 
 	local LINE
-	plan_hook_lines "$DB_FILE" "$PATTERN" | while read LINE; do
-		local REST=`echo $LINE | cut -d\  -f2-`
-		local TIME=`echo $LINE | awk '{ print $1 }'`
-		plan_add_entry -e -db "$DB_FILE" $TIME $REST
+	local CONVEYER="plan_read_lines | plan_hook_lines \"$DB_FILE\" \"$PATTERN\""
+	eval "$CONVEYER" | while read LINE; do
+		plan_add_entry -e -db "$DB_FILE" $LINE
 	done
 
 	mv "$DB_FILE" "$PLAN_DATABASE"
